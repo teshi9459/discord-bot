2cc2d185a51725fc3fa6de5255cd3921bf548222
Teshi <dj.mobile.h@gmail.com>
2022-09-26 07:24:30 +0200

    ready.js aus tesh-bot

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,15 @@ module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
-    console.log(`Ready! Logged in as ${client.user.tag}`);
+    const guilds = client.guilds.cache.map((guild) => guild.name);
+    console.log(`online as ${client.user.tag}\non ${guilds}`);
+    client.user.setPresence({
+      activities: [
+        {
+          name: 'auf ' + guilds.length + ' Servern',
+        },
+      ],
+      status: 'online',
+    });
   },
 };
